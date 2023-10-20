532bb51c03d19044b63f43babf7493c2232e4599
Caffery Yang <cafferychen7850@gmail.com>
2023-10-20 19:43:38 +0800

    Update long_server.R

--- server/long_server.R
+++ server/long_server.R
@@ -11,55 +11,85 @@ library(lmerTest)
 library(MicrobiomeStat)
 
 server_long <- function(input, output, session) {
+
   reportPath <- reactiveVal(NULL)
-  observeEvent(c(input$dataFile, input$countData, input$metadata, input$taxonomy), {
-    data.obj <- processDataFiles(input)
+
+  observeEvent(c(input$long_dataFile, input$long_countData, input$long_metadata, input$long_taxonomy), {
+
+    data.obj <- processDataFiles(input, "long")
+
     if (!is.null(data.obj)) {
-      updateDepthChoices(data.obj, session)
-      updateMetaDatChoices(data.obj, session)
-      updateT0LevelChoices(data.obj, input, session)
-      updateTSLevelsChoices(data.obj, input, session)
-      updateFeatureAnnChoices(data.obj, session)
+      updateDepthChoices(data.obj, session, "long_")
+
+      updateMetaDatChoices(data.obj, session, "long_")
+
+      updateT0LevelChoices(data.obj, input, session, "long_")
+
+      updateTSLevelsChoices(data.obj, input, session, "long_")
+
+      updateFeatureAnnChoices(data.obj, session, "long_")
     }
   })
 
-  observeEvent(input$runAnalysis, {
-    req(input$groupVar)
-    data.obj <- processDataFiles(input)
-    depth <- if (input$depth == "NULL") NULL else as.numeric(input$depth)
+  observeEvent(input$long_runAnalysis, {
+    req(input$long_groupVar)
+
+    data.obj <- processDataFiles(input, "long")
+
+    showModal(modalDialog(
+      title = "Analysis Started",
+      "The analysis has begun. Please be patient and wait.",
+      footer = NULL,
+      easyClose = FALSE
+    ))
+
+    # 根据input$single_featureDatType的值调整depth参数
+    depth_value <- if (input$long_featureDatType == "proportion") NULL else input$long_depth
+
     reportFile <- mStat_generate_report_long(data.obj = data.obj,
-                                             group.var = input$groupVar,
-                                             vis.adj.vars = input$visAdjVars,
-                                             test.adj.vars = input$testAdjVars,
-                                             strata.var = input$strataVar,
-                                             subject.var = input$subjectVar,
-                                             time.var = input$timeVar,
-                                             t0.level = input$t0Level,
-                                             ts.levels = input$tsLevels,
-                                             alpha.name = input$alphaName,
-                                             alpha.change.func = input$alphaChangeFunc,
-                                             depth = depth,
-                                             prev.filter = input$prevFilter,
-                                             abund.filter = input$abundFilter,
-                                             bar.area.feature.no = input$barAreaFeatureNo,
-                                             heatmap.feature.no = input$heatmapFeatureNo,
-                                             dotplot.feature.no = input$dotplotFeatureNo,
-                                             vis.feature.level = input$visFeatureLevel,
-                                             test.feature.level = input$testFeatureLevel,
-                                             feature.dat.type = input$featureDatType,
-                                             feature.change.func = input$featureChangeFunc,
-                                             feature.analysis.rarafy = input$featureAnalysisRarafy,
-                                             feature.mt.method = input$featureMtMethod,
-                                             feature.sig.level = input$featureSigLevel,
-                                             feature.box.axis.transform = input$featureBoxAxisTransform,
-                                             base.size = input$baseSize,
-                                             theme.choice = input$themeChoice,
-                                             output.file = input$outputFile)
+                                             group.var = input$long_groupVar,
+                                             vis.adj.vars = input$long_visAdjVars,
+                                             test.adj.vars = input$long_testAdjVars,
+                                             strata.var = input$long_strataVar,
+                                             subject.var = input$long_subjectVar,
+                                             time.var = input$long_timeVar,
+                                             t0.level = input$long_t0Level,
+                                             ts.levels = input$long_tsLevels,
+                                             alpha.name = input$long_alphaName,
+                                             alpha.change.func = input$long_alphaChangeFunc,
+                                             depth = depth_value,
+                                             prev.filter = input$long_prevFilter,
+                                             abund.filter = input$long_abundFilter,
+                                             bar.area.feature.no = input$long_barAreaFeatureNo,
+                                             heatmap.feature.no = input$long_heatmapFeatureNo,
+                                             dotplot.feature.no = input$long_dotplotFeatureNo,
+                                             vis.feature.level = input$long_visFeatureLevel,
+                                             test.feature.level = input$long_testFeatureLevel,
+                                             feature.dat.type = input$long_featureDatType,
+                                             feature.change.func = input$long_featureChangeFunc,
+                                             feature.analysis.rarafy = input$long_featureAnalysisRarafy,
+                                             feature.mt.method = input$long_featureMtMethod,
+                                             feature.sig.level = input$long_featureSigLevel,
+                                             feature.box.axis.transform = input$long_featureBoxAxisTransform,
+                                             base.size = input$long_baseSize,
+                                             theme.choice = input$long_themeChoice,
+                                             output.file = input$long_outputFile)
 
     reportPath(reportFile)
+
+    output$long_download_report_ui <- renderUI({
+      tags$div(
+        style = "color: green; margin-top: 20px; font-weight: bold;",
+        "Your long report is ready for download! ",
+        downloadLink('long_reportDownload', 'Click here to download')
+      )
+    })
+
+    removeModal()
+
   })
 
-  output$reportDownload <- downloadHandler(
+  output$long_reportDownload <- downloadHandler(
     filename = function() {
       paste0("MicrobiomeStat_Report_", Sys.Date(), ".pdf")
     },
